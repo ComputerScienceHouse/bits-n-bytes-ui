50c4cdbf83a90b072a92e28addcd35ac5c996f20
IsaacIngram <me@isaacingram.dev>
2024-09-09 09:57:18 -0400

    Initial commit of LVGL compiling

---
+++ ui/main.c
@@ -0,0 +1,14 @@
+//
+// File: main.c
+//
+// Purpose:
+//
+#include <stdio.h>
+#include "lvgl/lvgl.h"
+
+int main() {
+    // Initialize LVGL
+    lv_init();
+    printf("Initialized LVGL\n");
+    return 0;
+}
