7165134134823aae77c582820b77388f8232f5c6
Ajay Singh <aos2500@g.rit.edu>
2024-10-03 00:20:45 -0400

    making iniial UI changes

---
+++ ui/untitled/ui_form.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.3
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QSizePolicy, QWidget)
+
+class Ui_main(object):
+    def setupUi(self, main):
+        if not main.objectName():
+            main.setObjectName(u"main")
+        main.resize(800, 600)
+        self.checkBox = QCheckBox(main)
+        self.checkBox.setObjectName(u"checkBox")
+        self.checkBox.setGeometry(QRect(350, 240, 85, 20))
+
+        self.retranslateUi(main)
+
+        QMetaObject.connectSlotsByName(main)
+    # setupUi
+
+    def retranslateUi(self, main):
+        main.setWindowTitle(QCoreApplication.translate("main", u"main", None))
+        self.checkBox.setText(QCoreApplication.translate("main", u"CheckBox", None))
+    # retranslateUi
+
