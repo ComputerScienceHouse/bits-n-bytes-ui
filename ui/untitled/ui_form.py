2cea30bb5852e51868a98a3bcecdc19a51920dc7
Ajay Singh <aos2500@g.rit.edu>
2024-10-03 00:25:59 -0400

    init?

--- ui/untitled/ui_form.py
+++ ui/untitled/ui_form.py
@@ -15,24 +15,32 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QCheckBox, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QSizePolicy, QStackedWidget, QWidget)
 
 class Ui_main(object):
     def setupUi(self, main):
         if not main.objectName():
             main.setObjectName(u"main")
         main.resize(800, 600)
-        self.checkBox = QCheckBox(main)
-        self.checkBox.setObjectName(u"checkBox")
-        self.checkBox.setGeometry(QRect(350, 240, 85, 20))
+        self.Main = QStackedWidget(main)
+        self.Main.setObjectName(u"Main")
+        self.Main.setGeometry(QRect(20, 30, 751, 551))
+        self.page = QWidget()
+        self.page.setObjectName(u"page")
+        self.Main.addWidget(self.page)
+        self.page_2 = QWidget()
+        self.page_2.setObjectName(u"page_2")
+        self.Main.addWidget(self.page_2)
 
         self.retranslateUi(main)
+
+        self.Main.setCurrentIndex(0)
+
 
         QMetaObject.connectSlotsByName(main)
     # setupUi
 
     def retranslateUi(self, main):
         main.setWindowTitle(QCoreApplication.translate("main", u"main", None))
-        self.checkBox.setText(QCoreApplication.translate("main", u"CheckBox", None))
     # retranslateUi
 
