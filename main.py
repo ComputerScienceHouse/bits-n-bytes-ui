aa7e1ef0d48b1d623bfcb7539074e73108483d23
sahil <shp2382@g.rit.edu>
2024-11-06 22:38:32 -0500

    fixed mqtt in main.py

--- main.py
+++ main.py
@@ -6,14 +6,8 @@ from PySide6.QtCore import Qt
 from screens.cart_screen import CartScreen
 from screens.welcome_screen import WelcomeScreen
 from screens.reciept_screen import RecieptScreen
-from paho.mqtt.client import Client
+import mqtt
 import resources_rc  # Ensure your resources are compiled and available
-
-# MQTT settings
-broker = "test.mosquitto.org"  # Update with broker URI as needed
-port = 1883
-open_doors_topic = "aux/control/doors"
-open_doors_msg = "open"
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -21,13 +15,6 @@ class MainWindow(QMainWindow):
         self.setFixedSize(1024, 600)
         self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
         self.initUI()
-
-        # Set up MQTT client and callbacks
-        self.mqtt_client = Client()
-        self.mqtt_client.on_message = self.on_message  # Instance method as callback
-        self.mqtt_client.connect(broker, port)
-        self.mqtt_client.subscribe(open_doors_topic)
-        self.mqtt_client.loop_start()  # Start the loop in the background
 
     def initUI(self):
         # Create a QStackedWidget to manage different screens
@@ -50,17 +37,13 @@ class MainWindow(QMainWindow):
         self.stack.addWidget(self.reciept_screen)    # Index 2
 
         # Connect buttons for navigation (for debugging/development)
-        self.welcome_screen.ui.tapButton.clicked.connect(lambda: self.stack.setCurrentIndex(1))
+        self.welcome_screen.ui.tapButton.clicked.connect(lambda: self.go_to_cart)
         self.cart_screen.ui.navButton.clicked.connect(lambda: self.stack.setCurrentIndex(0))
         self.cart_screen.ui.navRecieptButton.clicked.connect(lambda: self.stack.setCurrentIndex(2))
 
-    def on_message(self, message):
-        payload = message.payload.decode()
-        print(f"Received message: {payload} on topic: {message.topic}")
-
-        # Check if the message indicates door status and switch to the cart screen
-        if message.topic == open_doors_topic and payload == open_doors_msg:
-            self.stack.setCurrentIndex(1)  # Switch to the cart screen (index 1)
+    def go_to_cart(self):
+        mqtt.open_doors()
+        widget.stack.setCurrentIndex(1)
 
 def load_stylesheet(theme):
     with open(f"resources/styles/{theme}_style.qss", "r") as file:
