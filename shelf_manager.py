efa2518325ec904837475844c9ca0d1902f9e86c
IsaacIngram <me@isaacingram.dev>
2024-11-16 02:01:51 -0500

    Add all items and shelves

--- shelf_manager.py
+++ shelf_manager.py
@@ -21,9 +21,9 @@ KNOWN_TARE_WEIGHT_G = 226.0
 
 SHELF_ITEM_MAP = {
     "MAC_1": [Slot([]), Slot([]), Slot([]), Slot([])],
-    "80:65:99:49:EF:8E": [ Slot([database.MOCK_ITEMS[9]]) , Slot([database.MOCK_ITEMS[1]]) , Slot([database.MOCK_ITEMS[9]]) , Slot([database.MOCK_ITEMS[6]]) ],
-    "80:65:99:E3:EF:50": [ Slot([]) , Slot([]) , Slot([]) , Slot([]) ],
-    "80:65:99:E3:8B:92": [ Slot([]) , Slot([]) , Slot([]) , Slot([]) ],
+    "80:65:99:49:EF:8E": [ Slot([database.MOCK_ITEMS[9]]) , Slot([database.MOCK_ITEMS[12]]) , Slot([database.MOCK_ITEMS[9]]) , Slot([database.MOCK_ITEMS[6]]) ],
+    "80:65:99:E3:EF:50": [ Slot([database.MOCK_ITEMS[12]]) , Slot([database.MOCK_ITEMS[12]]) , Slot([database.MOCK_ITEMS[12]]) , Slot([database.MOCK_ITEMS[12]]) ],
+    "80:65:99:E3:8B:92": [ Slot([database.MOCK_ITEMS[10]]) , Slot([database.MOCK_ITEMS[10]]) , Slot([database.MOCK_ITEMS[10]]) , Slot([database.MOCK_ITEMS[11]]) ],
 }
 NUM_SLOTS_PER_SHELF = 4
 LOOP_DELAY_MS = 200
