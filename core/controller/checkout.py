bd06c2d6422d6e0e72267c4d9f5ed03db2b39737
IsaacIngram <me@isaacingram.dev>
2025-04-23 18:04:20 -0400

    Add "ImagineRIT credit" to SMS receipt

--- core/controller/checkout.py
+++ core/controller/checkout.py
@@ -70,7 +70,7 @@ class CheckoutController(QObject):
             items.append(f"{item.quantity} {product}   ${item.price:.2f}")
         
         items_string = "\n".join(items)
-        msg = f"""Thank you for purchasing from\nBits 'n Bytes.\n\nOrder #1\n{items_string}\nSubtotal: ${self._model._cart.get_subtotal():.2f}\nTOTAL: ${self._model._cart.get_subtotal():.2f}
+        msg = f"""Thank you for purchasing from\nBits 'n Bytes.\n\nOrder #0\n{items_string}\nSubtotal: ${self._model._cart.get_subtotal():.2f}\nImagineRIT Credit: -${self._model._cart.get_subtotal():.2f}\nTOTAL: $0.00
         """
 
         client.messages.create(
